bb3d0200c8610a27f615aea602332935de13fded
chen sheng <328921371@qq.com>
2020-05-08 11:26:53 +0800

    2020/05/08

---
+++ src/main/java/com/example/demo/pojo/GreetingService.java
@@ -0,0 +1,10 @@
+package com.example.demo.pojo;
+
+/**
+ * @author `Administrator`
+ * @date:2020/5/8
+ */
+@FunctionalInterface
+public interface GreetingService {
+    void  sayMessage(String message);
+}
---
+++ src/test/java/com/example/demo/KK.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * @author `Administrator`
+ * @date:2020/5/8 000810:09
+ */
+@SpringBootTest
+public class KK {
+    @Test
+    void load(){
+        List<String>strings= Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+
+        List<String> filtered=strings.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+        System.out.println(filtered);
+
+        Random random=new Random();
+        /*创建ints流 。限制。遍历*/
+        random.ints().limit(10).forEach(System.out::println);
+
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+                /*map ,每个元素映射到》自己想要的结果（integer*integer）。筛选不同（可选参数）。转化成List*/
+        List<Integer>sq=numbers.stream().map(integer -> integer*integer).distinct().collect(Collectors.toList());
+        System.out.println(sq);
+
+        random.ints().limit(10).sorted().forEach(System.out::println);
+       numbers.stream().filter(integer -> integer > 5).collect(Collectors.toList()).forEach(integer -> System.out.println(integer));
+
+        long count = strings.parallelStream().filter(s -> s.isEmpty()).count();
+
+        System.out.println("长度："+count);
+        IntSummaryStatistics summaryStatistics = numbers.stream().mapToInt(x -> x).summaryStatistics();
+        System.out.println("最大数"+summaryStatistics.getMax());
+        System.out.println("最小数"+summaryStatistics.getMin());
+        System.out.println("和"+summaryStatistics.getSum());
+        System.out.println("平均数"+summaryStatistics.getAverage());
+        System.out.println("个数"+summaryStatistics.getCount());
+    }
+}
+
